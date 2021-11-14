9858efbeec2400fe217237f3591abd740a91566d
Miguel Escalante <mickey27esc@gmail.com>
2021-11-13 20:18:04 -0600

    Web first update
    Se establece la base de la app web

--- web.rkt
+++ web.rkt
@@ -2,39 +2,61 @@
  
 ; A blog is a (listof post)
 ; and a post is a (post title body)
-(struct post (title body))
+(struct problem (description solution))
  
 ; BLOG: blog
 ; The static blog.
-(define BLOG
-  (list (post "Second Post" "This is another post")
-        (post "First Post" "This is my first post")))
+(define SET
+  (list (problem "Descr 1" "solu 1")
+        (problem "Descr 2" "solu 2")))
  
 ; start: request -> response
-; Consumes a request, and produces a page that displays all of the
+; Consumes a request and produces a page that displays all of the
 ; web content.
 (define (start request)
-  (render-blog-page BLOG request))
+  (define a-set
+    (cond [(can-parse-problem? (request-bindings request))
+           (cons (parse-problem (request-bindings request))
+                 SET)]
+          [else
+           SET]))
+  (render-set-page a-set request))
+ 
+ 
+; can-parse-post?: bindings -> boolean
+; Produces true if bindings contains values for 'title and 'body.
+(define (can-parse-problem? bindings)
+  (and (exists-binding? 'description bindings)
+       (exists-binding? 'solution bindings)))
+ 
+ 
+; parse-post: bindings -> post
+; Consumes a bindings, and produces a post out of the bindings.
+(define (parse-problem bindings)
+  (problem (extract-binding/single 'description bindings)
+           (extract-binding/single 'solution bindings)))
  
 ; render-blog-page: blog request -> response
 ; Consumes a blog and a request, and produces an HTML page
 ; of the content of the blog.
-(define (render-blog-page a-blog request)
+(define (render-set-page a-set request)
   (response/xexpr
-   `(html (head (title "My Blog"))
-          (body (h1 "My Blog")
-                ,(render-posts a-blog)
-           )
-    )
-   )
-  )
+   `(html (head (title "SET of Problems"))
+          (body
+           (h1 "Set of problems")
+           ,(render-problems a-set)
+           (form
+            (input ((name "description")))
+            (input ((name "solution")))
+            (input ((type "submit"))))))))
  
 ; render-post: post -> xexpr
 ; Consumes a post, produces an xexpr fragment of the post.
-(define (render-post a-post)
-  `(div ((class "post"))
-        ,(post-title a-post)
+(define (render-problem a-prob)
+  `(div ((class "problem"))
+        ,(problem-title a-post)
         (p ,(post-body a-post))))
+ 
  
 ; render-posts: blog -> xexpr
 ; Consumes a blog, produces an xexpr fragment
